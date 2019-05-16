69fdcf672a1e1f421408c8c83c53ee85eba675f3
West Lane <west@paperequator.com>
2019-05-16 07:54:35 -0400

    New Style for Changed vs. Focused Markers

--- mapify/app.js
+++ mapify/app.js
@@ -128,7 +128,7 @@
                 c= [c[0], c[0], c[1], c[1], c[2], c[2]];
             }
             c= '0x'+c.join('')
-            return 'rgba('+[(c>>16)&255, (c>>8)&255, c&255].join(',')+',0.5)'
+            return 'rgba('+[(c>>16)&255, (c>>8)&255, c&255].join(',')+',0.9)'
         }
         throw new Error('Bad Hex')
     }
