605bad4035a441e16ef12ed32f9f4a642adc98f5
West Lane <west@paperequator.com>
2019-06-03 16:41:06 -0400

    Inject Manifest For App

--- theme/app.js
+++ theme/app.js
@@ -1,4 +1,10 @@
 (function () {
-    let Component = {}
+    let Component = {
+    }
+    // inject manifest
+    var importTag = document.createElement('link');
+    importTag.setAttribute('rel', 'manifest');
+    importTag.setAttribute('href', '/-/theme/manifest.json');
+    document.head.appendChild(importTag);
     return Component
 }())
